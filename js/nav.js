6111f3f65aa0e50c3d02476d1af43ffdea7dfd86
Virgilio Baylosis Ricaro Jr <ricarovirgiliojr@gmail.com>
2020-07-19 08:33:28 +0800

    complete styling
    with script for mobile navigation

---
+++ js/nav.js
@@ -0,0 +1,41 @@
+let navMobileBtn = document.querySelector('.nav--mobile__button')
+let navMobileBtnBG = document.querySelector('.nav--mobile__button > div');
+let navMobile = document.querySelector('.nav--mobile')
+let indexBody = document.querySelector('body')
+
+let navDisplay = false
+
+let navIsOpen = () => {
+    navMobileBtnBG.style.backgroundImage = `url(images/icon-close.svg)`
+    navMobileBtnBG.style.animation = `rotateCounterClockWise 400ms 1`
+    navMobileBtnBG.style.transition = `background-image 200ms`
+
+    navMobile.style.opacity = 1
+    navMobile.style.left = 0
+    navMobile.style.zIndex = 100
+
+    indexBody.style.overflowY = `hidden`
+
+    navDisplay = true
+}
+
+let navIsClose = () => {
+    navMobileBtnBG.style.backgroundImage = `url(images/icon-hamburger.svg)`
+    navMobileBtnBG.style.animation = `rotateClockWise 400ms 1`
+    navMobileBtnBG.style.transition = `background-image 400ms 100ms`
+
+    navMobile.style.opacity = 0
+    navMobile.style.left = `100%`
+
+    setTimeout(() => {
+        navMobile.style.zIndex = -9999
+    }, 600) 
+
+    indexBody.style.overflowY = `visible`
+
+    navDisplay = false
+}
+
+navMobileBtn.addEventListener("click", () => {
+    (navDisplay ? navIsClose() : navIsOpen())
+})
